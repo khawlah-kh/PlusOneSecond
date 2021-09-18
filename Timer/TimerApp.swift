c1d9de5ca1f94d630c9543afac6e422571425f08
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2021-09-18 14:41:29 +0300

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by khawlah khalid on 18/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
